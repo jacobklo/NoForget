cf74664b32c0296d61cd57ef6b450ca3157a7f90
Jacob Lo <jljacoblo@gmail.com>
2018-09-21 16:24:13 -0500

    create a clear button for unsaved entry

--- mobile/src/main/java/net/jacoblo/noforget/MainActivity.kt
+++ mobile/src/main/java/net/jacoblo/noforget/MainActivity.kt
@@ -1,30 +1,16 @@
 package net.jacoblo.noforget
 
 import android.os.Bundle
-import android.os.Environment
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 class MainActivity : AppCompatActivity() {
 
   val LOG_TAG = "NoForget Log"
   var m_CurrentMemoryEntryCount = 0;
   val m_MemoryData = MemoryData(0, ArrayList<MemoryEntry>() )
-  val m_DefaultReminderDates = createDefaultReminderDates()
 
   private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
     setVisibilities(item.itemId)
@@ -59,80 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-    create_save.setOnClickListener { _ : View? ->
-      saveToFile(LocalDate.now().toString()+".txt",memoryDataToJson(m_MemoryData))
-    }
-
-    create_new.setOnClickListener{ _ : View? ->
-      createNewMemoryEntry()
-    }
-
-    create_add_date.setOnClickListener { _ : View? ->
-      createNewDateSubView( m_DefaultReminderDates.size )
-    }
-
+    onCreateCreatePart()
   }
 
-  private fun convertDatesViewGroupToDatesArray( dateTimeGroup: ViewGroup): ArrayList<LocalDateTime>  {
-
-    val result = ArrayList<LocalDateTime>()
-
-    for ( i in 0..dateTimeGroup.childCount ) {
-
-      // check if actually LinearLayout
-      if ( dateTimeGroup.getChildAt(i) !is LinearLayout) {
-        return result; //Empty as not dateTimeGroup
-      }
-
-      val child = dateTimeGroup.getChildAt(i) as LinearLayout;
-      val dateEditText = child.getChildAt(0) as EditText;
-      val timeEditText = child.getChildAt(1) as EditText;
-
-      Toast.makeText(this,dateEditText.text.toString() + " " + timeEditText.text.toString(), Toast.LENGTH_LONG).show()
-      val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.nnn")
-      try {
-        val childLocalDateTime = LocalDateTime.parse(dateEditText.text.toString() + " " + timeEditText.text.toString(), dateTimeFormatter)
-        result.add(childLocalDateTime)
-      }catch ( dtpe: DateTimeParseException) {}
-    }
-    return result
-  }
-
-  private fun createNewMemoryEntry() {
-
-    var newMemoryEntry: MemoryEntry = MemoryEntry(m_CurrentMemoryEntryCount
-                                                , LocalDateTime.now()
-            , findViewById<EditText>(R.id.create_name).text.toString()
-            , convertDatesViewGroupToDatesArray(datesContainer)
-            , findViewById<EditText>(R.id.create_data).text.toString()
-    )
-    m_CurrentMemoryEntryCount++
-    m_MemoryData.memory_entries.add(newMemoryEntry)
-  }
-
-  private fun createDefaultReminderDates(): ArrayList<LocalDateTime> {
-    val result = ArrayList<LocalDateTime>()
-    result.add(LocalDateTime.now().plusDays(1))
-    result.add(LocalDateTime.now().plusDays(3))
-    result.add(LocalDateTime.now().plusDays(7))
-    result.add(LocalDateTime.now().plusDays(30))
-    result.add(LocalDateTime.now().plusDays(90))
-    result.add(LocalDateTime.now().plusDays(365))
-    return result
-  }
-
-  private fun saveToFile(fileName: String, fileContents: String) {
-    val saveFileDir = File(Environment.getExternalStorageDirectory(), "NoForget")
-    if (!saveFileDir.exists() && !saveFileDir.mkdir()) {
-      Log.e(LOG_TAG, "cannot create save file for NoForget")
-      Toast.makeText(applicationContext, "ERROR: cannot save, please able storage permission.", Toast.LENGTH_LONG).show()
-    }
-
-    val saveFilePath = saveFileDir.absolutePath + "/" + fileName
-    val fos = FileOutputStream(saveFilePath)
-    val osw = OutputStreamWriter(fos)
-    osw.write(fileContents)
-    osw.close()
-
-  }
 }
--- mobile/src/main/java/net/jacoblo/noforget/MainActivitySubpartCreate.kt
+++ mobile/src/main/java/net/jacoblo/noforget/MainActivitySubpartCreate.kt
@@ -3,14 +3,108 @@ package net.jacoblo.noforget
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
+import android.os.Environment
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 // REMEMBER : Use Kotline Extension capability to seperate functions to another file
+val MainActivity.m_DefaultReminderDates: ArrayList<LocalDateTime>
+    get() = createDefaultReminderDates()
+
+internal fun MainActivity.createDefaultReminderDates(): ArrayList<LocalDateTime> {
+  val result = ArrayList<LocalDateTime>()
+  result.add(LocalDateTime.now().plusDays(1))
+  result.add(LocalDateTime.now().plusDays(3))
+  result.add(LocalDateTime.now().plusDays(7))
+  result.add(LocalDateTime.now().plusDays(30))
+  result.add(LocalDateTime.now().plusDays(90))
+  result.add(LocalDateTime.now().plusDays(365))
+  return result
+}
+
+internal fun MainActivity.onCreateCreatePart() {
+  create_save.setOnClickListener { _ : View? ->
+    saveToFile(LocalDate.now().toString()+".txt",memoryDataToJson(m_MemoryData))
+  }
+
+  create_new.setOnClickListener{ _ : View? ->
+    createNewMemoryEntry()
+  }
+
+  create_add_date.setOnClickListener { _ : View? ->
+    createNewDateSubView( m_DefaultReminderDates.size )
+  }
+
+  create_delete.setOnClickListener{ _ : View? ->
+    clearUnsavedEntry()
+  }
+}
+
+internal fun MainActivity.saveToFile(fileName: String, fileContents: String) {
+  val saveFileDir = File(Environment.getExternalStorageDirectory(), "NoForget")
+  if (!saveFileDir.exists() && !saveFileDir.mkdir()) {
+    Log.e(LOG_TAG, "cannot create save file for NoForget")
+    Toast.makeText(applicationContext, "ERROR: cannot save, please able storage permission.", Toast.LENGTH_LONG).show()
+  }
+
+  val saveFilePath = saveFileDir.absolutePath + "/" + fileName
+  val fos = FileOutputStream(saveFilePath)
+  val osw = OutputStreamWriter(fos)
+  osw.write(fileContents)
+  osw.close()
+
+}
+
+internal fun MainActivity.createNewMemoryEntry() {
+
+  var newMemoryEntry: MemoryEntry = MemoryEntry(m_CurrentMemoryEntryCount
+          , LocalDateTime.now()
+          , findViewById<EditText>(R.id.create_name).text.toString()
+          , convertDatesViewGroupToDatesArray(datesContainer)
+          , findViewById<EditText>(R.id.create_data).text.toString()
+  )
+  m_CurrentMemoryEntryCount++
+  m_MemoryData.memory_entries.add(newMemoryEntry)
+
+  // clear
+  clearUnsavedEntry()
+}
+
+
+internal fun MainActivity.convertDatesViewGroupToDatesArray( dateTimeGroup: ViewGroup): ArrayList<LocalDateTime>  {
+
+  val result = ArrayList<LocalDateTime>()
+
+  for ( i in 0..dateTimeGroup.childCount ) {
+
+    // check if actually LinearLayout
+    if ( dateTimeGroup.getChildAt(i) !is LinearLayout) {
+      return result; //Empty as not dateTimeGroup
+    }
+
+    val child = dateTimeGroup.getChildAt(i) as LinearLayout;
+    val dateEditText = child.getChildAt(0) as EditText;
+    val timeEditText = child.getChildAt(1) as EditText;
+
+    val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    try {
+      val childLocalDateTime = LocalDateTime.parse(dateEditText.text.toString() + " " + timeEditText.text.toString(), dateTimeFormatter)
+      result.add(childLocalDateTime)
+    }catch ( _ : DateTimeParseException) {}
+  }
+  return result
+}
 
 internal fun MainActivity.createNewDateSubView( defaultDatesSize: Int ) {
   val datesLayout = findViewById<LinearLayout>(R.id.datesContainer)
@@ -22,13 +116,15 @@ internal fun MainActivity.createNewDateSubView( defaultDatesSize: Int ) {
   dateTimeGroup.layoutParams = dateTimeGroupLL
   datesLayout?.addView(dateTimeGroup)
 
-  val defaultNewDate = if (numOfDates < defaultDatesSize ) m_DefaultReminderDates[numOfDates].toLocalDate().toString() else "Date"
+  var dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+  val defaultNewDate = if (numOfDates < defaultDatesSize ) m_DefaultReminderDates[numOfDates].format(dateTimeFormatter) else "Date"
   val newDateEditText = createCustomPickerEditText(this, defaultNewDate, 0.30f ) {
     newPickerEditText: EditText ->
     createDatePickerDialog(LocalDateTime.now().plusDays(1), newPickerEditText).show()
   }
 
-  val defaultNewTime = if (numOfDates < defaultDatesSize ) m_DefaultReminderDates[numOfDates].toLocalTime().toString() else "Time"
+  dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+  val defaultNewTime = if (numOfDates < defaultDatesSize ) m_DefaultReminderDates[numOfDates].format(dateTimeFormatter) else "Time"
   val newTimeEditText = createCustomPickerEditText(this, defaultNewTime, 0.30f ) {
     newPickerEditText: EditText ->
     createTimePickerDialog(LocalDateTime.now(), newPickerEditText).show()
@@ -80,4 +176,10 @@ internal fun MainActivity.createDatePickerDialog(defaultDate: LocalDateTime, dat
   }
   // REMEMBER : month and day started at 1
   return DatePickerDialog(this, lis, defaultDate.year, defaultDate.monthValue - 1, defaultDate.dayOfMonth - 1)
+}
+
+internal fun MainActivity.clearUnsavedEntry() {
+  create_name.text = null
+  create_data.text = null
+  datesContainer.removeAllViews()
 }
